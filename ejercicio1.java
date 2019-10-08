a8b0248d900dce4c3bd40f4fa21d133aec0deaac
Sverde-dot <55793525+Sverde-dot@users.noreply.github.com>
2019-10-08 08:58:01 +0200

    Add files via upload

---
+++ ejercicio1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EJ1;
+
+/**
+ *
+ * @author sverdecadilla
+ */
+public class ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int b=4,h=3,A;
+        A=(b*h)/2;
+        System.out.println("La area de un triangulo de base "+b+" y altura "+h+" es igual a "+A);
+        
+        
+        
+        
+    }
+    
+}
